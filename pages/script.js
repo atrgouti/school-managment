d3470ae10ef6161ebe8949d23aed8d4527359635
atrgouti <bilalatrgouti2002@gmail.com>
2023-06-04 19:05:26 +0200

    still working on the design

--- pages/script.js
+++ pages/script.js
@@ -10,3 +10,19 @@ close_btn.addEventListener('click', () => {
     nav.forEach(nav_el => nav_el.classList.remove('visible'))
 })
 
+
+const valueDisplays = document.querySelectorAll(".nums")
+// let interval = 100;
+
+valueDisplays.forEach((value) => {
+    let startValue = 0
+    let endValue = parseInt(value.getAttribute("data-val"))
+    // let duration = Math.floor(interval / endValue)
+    let counter = setInterval(function() {
+        startValue += 1
+        value.textContent = startValue;
+        if(startValue == endValue){
+            clearInterval(counter);
+        }
+    }, 1)
+})
